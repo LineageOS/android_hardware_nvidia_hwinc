194c6fb89d0bdbe7bde22f65d33a6adc4f6f493c
Aaron Kling <webgeek1234@gmail.com>
2022-12-04 01:28:02 -0600

    Update t19x from spe 35.1
    Change-Id: I5f00b037d9013e33c7a25b2e503d1a9d7fc1de69

---
+++ t19x/41334769/tcu-priv.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2020-2022, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef TCU_PRIV_H
+#define TCU_PRIV_H
+
+#include <stdint.h>
+
+#define rtos_task_define_s(_t_, _fn_, _name_, _prio_, _params_,_stack_depth_) \
+    static const rtosTaskParameters _t_##_params = \
+    {                                              \
+        .pvTaskCode         = & _fn_,              \
+        .pcTaskName         = _name_,              \
+        .uxPriority         = _prio_,              \
+        .pvParameters       = _params_,            \
+        .uxStackDepthBytes  = _stack_depth_        \
+    }
+
+#define TCU_ID_CCPLEX           (0U)
+#define TCU_ID_SCE              (1U)
+#define TCU_ID_TZ               (2U)
+#define TCU_ID_BPMP             (3U)
+#define TCU_ID_RCE              (4U)
+
+#define TCU_TAG_MIN             (0xe1U)
+#define TCU_TAG_CCPLEX          (0xe1U)
+#define TCU_TAG_BPMP            (0xe2U)
+#define TCU_TAG_SCE             (0xe3U)
+#define TCU_TAG_TZ              (0xe4U)
+#define TCU_TAG_RCE             (0xe5U)
+#define TCU_TAG_MAX             (0xe5U)
+
+#define SPE_NUM_MSGS            (16UL)
+#define SPE_MSG_SIZE            (128UL)
+#define EXT_CLIENT_MSG_SIZE     (256UL)
+#define EXT_CLIENT_RX_BUF_SIZE  (256UL)
+
+#define TCU_TX_QUEUE_SIZE rtos_queue_size(EXT_CLIENT_MSG_SIZE, \
+                                          sizeof(uint32_t))
+#define TCU_RX_QUEUE_SIZE rtos_queue_size(EXT_CLIENT_RX_BUF_SIZE, 1)
+
+#endif
